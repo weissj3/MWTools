352d0a7a6ffdb49282f0ffe6bdc866586ba11b79
weissj3 <weissj3@rpi.edu>
2018-03-01 10:48:18 -0500

    Updated 3D Motion Coordinate Transform

--- Scripts/3DMotion.py
+++ Scripts/3DMotion.py
@@ -4,8 +4,34 @@ import matplotlib
 import matplotlib.pyplot as plt
 from astropy.coordinates import SkyCoord
 import astropy.units as u
+from astropy.io import ascii
+from operator import mul
 
-r = 10. ** (20.337 - 4.2)/5.0 / 100.
-sc = SkyCoord(ra=191.9772186*u.deg, dec=-8.5618715*u.deg, distance=r*u.kpc, pm_ra_cosdec=10.6677*u.mas/u.yr, pm_dec=-16.4151*u.mas/u.yr, radial_velocity=146.2*u.km/u.s)
+def distance(g_0):
+    return (10. ** ((g_0 - 4.2)/5.0) / 100.)*u.kpc 
 
-print(sc.cartesian)
+
+data = ascii.read("../../Segue_weissj3_GPS1.csv", format='csv', fast_reader=True)
+
+sc = SkyCoord(ra=[i*u.deg for i in data["ra_2"] ], dec=[i*u.deg for i in data["dec_2"]], distance=[distance(i) for i in data["g0"]], pm_ra_cosdec=[i*u.mas/u.yr for i in data["mura"]], pm_dec=[i * u.mas/u.yr for i in data["mudec"]], radial_velocity=[i * u.km/u.s for i in data["elodierv"]])
+
+print(sc.velocity)
+plt.subplot(2,2,1)
+plt.plot(sc.velocity.d_x, sc.velocity.d_y, "o", ms=0.5)
+plt.xlim([-2000, 2000])
+plt.ylim([-2000, 2000])
+plt.xlabel("X Velocity (km/s)")
+plt.ylabel("Y Velocity (km/s)")
+plt.subplot(2,2,2)
+plt.plot(sc.velocity.d_y, sc.velocity.d_z, "o", ms=0.5)
+plt.xlim([-2000, 2000])
+plt.ylim([-2000, 2000])
+plt.xlabel("Y Velocity (km/s)")
+plt.ylabel("Z Velocity (km/s)")
+plt.subplot(2,2,3)
+plt.plot(sc.velocity.d_x, sc.velocity.d_z, "o", ms=0.5)
+plt.xlim([-2000, 2000])
+plt.ylim([-2000, 2000])
+plt.xlabel("X Velocity (km/s)")
+plt.ylabel("Z Velocity (km/s)")
+plt.show()
